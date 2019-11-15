5d614ca33e42ad08352c2ff7949fa2fd93d2aafd
Christine Frank <christine.braun13@gmail.com>
2019-11-15 08:06:27 -0800

    Initialize project with config files and basic structure

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path='/' component={Home} />
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
