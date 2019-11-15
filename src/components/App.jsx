3adea412f45ef50706144851f2124e5391916eb7
Christine Frank <christine.braun13@gmail.com>
2019-11-15 09:52:26 -0800

    Add routes to App. Create header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,11 @@ function App() {
             <Header />
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/wine' component={WineList} />
+                <Route exact path='/food' component={FoodList} />
+                <Route exact path='/update-food' component={UpdateFoodControl} />
+                <Route exact path='/wine/new-wine' component={AddWineControl} />
+                {/* <Route exact path='/' component={Home} />    >>>>>>> Detail view route? */}
             </Switch>
         </div>
     );
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header(){
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="/">Why Not Wine</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" href="/wine">Wine List</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/food">Food Menu</Link>
+                        </li>
+                    </ul>
+            </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
