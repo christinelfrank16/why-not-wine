4f8b0d631bfeb795753d59fe573d12d250ef79ba
Christine Frank <christine.braun13@gmail.com>
2019-11-15 10:10:16 -0800

    correct indentations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,19 +5,19 @@ import Home from './Home';
 
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/wine' component={WineList} />
-                <Route exact path='/food' component={FoodList} />
-                <Route exact path='/update-food' component={UpdateFoodControl} />
-                <Route exact path='/wine/new-wine' component={AddWineControl} />
-                {/* <Route exact path='/' component={Home} />    >>>>>>> Detail view route? */}
-            </Switch>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/wine' component={WineList} />
+        <Route exact path='/food' component={FoodList} />
+        <Route exact path='/update-food' component={UpdateFoodControl} />
+        <Route exact path='/wine/new-wine' component={AddWineControl} />
+        {/* <Route exact path='/' component={Home} />    >>>>>>> Detail view route? */}
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
