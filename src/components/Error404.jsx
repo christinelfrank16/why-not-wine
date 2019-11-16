cb782431015db70db29d216ad6776550836ea08d
Christine Frank <christine.braun13@gmail.com>
2019-11-15 16:05:46 -0800

    style error page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function Error404(props) {
+  var textStyle = {
+    color: '#d1d1d1',
+    height: '100vh',
+    padding: '30px'
+  };
   return (
-    <div>
+    <div style={textStyle}>
       <h4>Attempted: {props.location.pathname}</h4>
+      <br/>
       <h2>The page you are looking for does not exist!</h2>
       <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
     </div>
