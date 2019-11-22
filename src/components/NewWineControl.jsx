22c807936959d5ce73185d3ab3133c7e27bd3ac7
Christine Frank <christine.braun13@gmail.com>
2019-11-22 14:35:39 -0800

    Add proptypes to newWineControl. (leaving for appointment)

--- src/components/NewWineControl.jsx
+++ src/components/NewWineControl.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import NewWineForm from './NewWineForm';
 import Confirmation from './Confirmation';
 
-function NewWineControl(){
+function NewWineControl(props){
   return (
     <div className='container'>
       <NewWineForm />
       <Confirmation />
     </div>
   );
+}
+
+NewWineControl.propTypes = {
+  
 }
 export default NewWineControl;
