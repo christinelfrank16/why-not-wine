75245c1b25a229dbae784e83eed76243126f500f
Christine Frank <christine.braun13@gmail.com>
2019-11-22 16:42:09 -0800

    Update react packages to enable use of hooks

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Confirmation(){
+function Confirmation(props){
+  function confirmValues(){
+    let html = <h3>Is this correct?</h3>;
+    props.valuesToConfirm.map((wineAndDetails) => {
+      Object.keys(wineAndDetails).forEach((key) => {
+        html += <p>{key} : {wineAndDetails[key]}</p>
+      });
+    });
+    return html;
+  }
   return(
-    <div className="modal fade" id="confirm" tabIndex="-1" role="dialog" aria-labelledby="confirmTitle" aria-hidden="true">
+    <div className="modal fade" id="confirm" show={props.showModal} role="dialog" aria-labelledby="confirmTitle" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLongTitle">Please Confirm this Action</h5>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <h5 className="modal-title" id="exampleModalLongTitle">Please Confirm this Wine</h5>
+            <button type="button" className="close" onClick={() => updateShowModal(false)} aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div className="modal-body">
-            <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-            <button type="button" className="btn btn-primary">Confirm</button>
+            {confirmValues()}
+            <button type="button" className="btn btn-secondary" onClick={() => updateShowModal(false)}>Cancel</button>
+            <button type="button" className="btn btn-primary" onClick={() => onWineConfirmed(props.valuesToConfirm)}>Confirm</button>
           </div>
         </div>
       </div>
     </div>
   );
+}
+
+Confirmation.propTypes = {
+  valuesToConfirm: PropTypes.array,
+  showModal: PropTypes.bool,
+  onWineConfirmed: PropTypes.func,
+  updateShowModal: PropTypes.func
 }
 
 export default Confirmation;
--- src/components/NewWineControl.jsx
+++ src/components/NewWineControl.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import NewWineForm from './NewWineForm';
 import Confirmation from './Confirmation';
 
-function NewWineControl(props){
+function NewWineControl(props) {
+  const [showModal, setShowModal] = useState(false);
+  const [confirmValues, setValues] = useState(null);
+
+  function handleNewWineInfoForConfirm(newWineObj){
+    setValues(newWineObj);
+    setShowModal(true);
+  }
+
   return (
     <div className='container'>
-      <NewWineForm />
-      <Confirmation />
+      <NewWineForm onNewWineCreation={handleNewWineInfoForConfirm}/>
+      <Confirmation valuesToConfirm={confirmValues} showModal={showModal} updateShowModal={setShowModal} onWineConfirmed={props.onWineConfirmed}/>
     </div>
   );
 }
 
+
 NewWineControl.propTypes = {
-  
+  onWineConfirmed: PropTypes.func
 }
 export default NewWineControl;
